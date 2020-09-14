f6af7cb830797cfb6ebee908fc014df92ee41f24
Aahan Singh Charak <aahancharak@gmail.com>
2020-09-14 12:01:30 +0530

    Added the sidebar

--- Version 2/Spotify Design/js/script.js
+++ Version 2/Spotify Design/js/script.js
@@ -1,6 +1,8 @@
 const ham = document.querySelector(".sidebar__toggle");
+const sidebar = document.querySelector(".main__sidebar");
 
 //adding event listener
 ham.addEventListener("click", (e) => {
   ham.classList.toggle("close");
+  sidebar.classList.toggle("active");
 });
